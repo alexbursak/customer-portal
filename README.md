a71092525e348945708fd62196d2c7fa067e6ee0
alexbursak <alexbursak3@gmail.com>
2017-02-25 11:10:01 +0000

    [INITIAL COMMIT] - symfony basic project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+customer-portal
+===============
+
+A Symfony project created on February 25, 2017, 10:40 am.
---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AB\CustomerPortal\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+}
