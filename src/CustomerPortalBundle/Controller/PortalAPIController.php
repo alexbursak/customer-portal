6ab55e9b2984aa56776f18c04c554aea133c6c4e
alexbursak <alexbursak3@gmail.com>
2017-02-26 19:01:44 +0000

    [UPDATE] - implemented basic ajax calls for Passenger

---
+++ src/CustomerPortalBundle/Controller/PortalAPIController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AB\CustomerPortalBundle\Controller;
+
+
+use AB\CustomerPortalBundle\Entity\Passenger;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class PortalAPIController extends Controller
+{
+    /**
+     * @return JsonResponse - Collection of Passengers
+     */
+    public function getPassengersAction()
+    {
+        $passengers = $this->get('security.token_storage')
+            ->getToken()
+            ->getUser()
+            ->getPassengers();
+
+        $response = [];
+        foreach ($passengers as $key => $value) {
+            $passenger['id'] = $value->getId();
+            $passenger['title'] = $value->getTitle();
+            $passenger['name'] = $value->getName();
+            $passenger['surname'] = $value->getSurname();
+            $passenger['passportID'] = $value->getpassportID();
+
+            array_push($response, (object)$passenger);
+        }
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function addPassengerAction(Request $request)
+    {
+        $customer = $this->get('security.token_storage')
+            ->getToken()
+            ->getUser();
+
+        $passengerRaw = $request->getContent();
+        $passenger = json_decode($passengerRaw);
+
+        if (empty($passengerRaw) || json_last_error() !== JSON_ERROR_NONE) {
+            return new JsonResponse('ERROR');
+        }
+
+        // persist passenger
+        $em = $this->getDoctrine()->getManager();
+        $result = $em->getRepository('CustomerPortalBundle:Passenger')
+            ->insertNewPassenger($passenger, $customer);
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function delPassengerAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $result = $em->getRepository('CustomerPortalBundle:Passenger')
+            ->deletePassenger($id);
+
+        return new JsonResponse($result);
+    }
+
+}
