97f379847753a771d1b9567cf1cdb877c051caca
alexbursak <alexbursak3@gmail.com>
2017-02-28 00:26:33 +0000

    [UPDATE] - passenger js (WIP)

--- src/CustomerPortalBundle/Resources/public/js/main.js
+++ src/CustomerPortalBundle/Resources/public/js/main.js
@@ -10,3 +10,37 @@ function callAjaxGET(url, successCallback) {
     });
 }
 
+function callAjaxPOST(url, data, successCallback) {
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        contentType: "application/json",
+        success: successCallback,
+        error: function () {
+            console.log('ERROR');
+        }
+    });
+}
+
+function callAjaxDELETE(url, successCallback) {
+
+    $.ajax({
+        type: 'DELETE',
+        url: url,
+        success: successCallback,
+        error: function () {
+            console.log('ERROR');
+        }
+    });
+}
+
+window.onclick = function (event) {
+
+    var modal = document.getElementById('CustomerModal');
+
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
